6763add5ccbaabd83cf608d3381bfae3d8e6cb09
muneebsafdar <safdarmuneeb95@gmail.com>
2025-10-25 15:07:55 +0500

    Update main dashboard page

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import FormDialogue from './_components/FormDialogue'
+import CreatedForms from './_components/CreatedForms'
 
 export default function dashboard() {
+
+  
   return (
-    <div>dashboard</div>
+    <div className='flex flex-col'>
+     <div className='py-6 px-10 flex justify-between items-center'>
+      <h2 className='text-3xl font-black'>Dashboard</h2>
+      <FormDialogue/>
+     </div>
+     <div>
+      <CreatedForms/>
+     </div>
+    </div>
   )
 }
+
+
+
+
