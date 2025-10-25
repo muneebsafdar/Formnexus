f56c5e26c02e20efae763280bdfbbffdd93d7236
muneebsafdar <safdarmuneeb95@gmail.com>
2025-10-25 15:05:59 +0500

    Update dashboard layout structure

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,13 +1,24 @@
 "use client"
 import { SignedIn, SignIn } from '@clerk/nextjs'
 import React from 'react'
+import SideNav from "@/app/dashboard/_components/SideNav"
 
 export default function DashboradLayout({children}) {
+
+  
   return (
-    <div>
      <SignedIn>
-         {children}
-     </SignedIn>
-    </div>
+
+      <div className='flex '>
+
+        <div className='md:w-64'>
+          <SideNav/>
+        </div>
+        <div className='w-full overflow-y-scroll no-scrollbar  h-screen '>
+            {children}
+        </div>
+
+      </div>
+    </SignedIn>
   )
 }
